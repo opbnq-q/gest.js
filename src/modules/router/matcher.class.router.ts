ff81a60008f58b2ec8944d7f577b806e49897624
opbnq-q <cko.vladislav@gmail.com>
2026-02-09 18:57:35 +0700

    add params

--- src/modules/router/matcher.class.router.ts
+++ src/modules/router/matcher.class.router.ts
@@ -4,22 +4,38 @@ export class Matcher {
   static match(route: string, path: string): MatchResult {
     const matchResult = new MatchResult(false);
     const url = new URL(route);
-    const { pathname } = url;
+    const { pathname, searchParams } = url;
+
     const splittedPath = path.split("/").filter(Boolean);
     const splittedRoute = pathname.split("/").filter(Boolean);
+
     if (splittedPath.length !== splittedRoute.length) {
       return matchResult;
     }
+
     for (let i = 0; i < splittedPath.length; i++) {
       const pathSegment = splittedPath[i];
       const routeSegment = splittedRoute[i];
+
       if (pathSegment.startsWith("[") && pathSegment.endsWith("]")) {
+        const key = pathSegment.slice(1, -1);
+        if (key) {
+          matchResult.pathParams.setParam(key, routeSegment);
+        }
         continue;
       }
+
       if (pathSegment !== routeSegment) {
         return matchResult;
       }
     }
+
+    if (searchParams && Array.from(searchParams.keys()).length > 0) {
+      for (const [key, value] of searchParams.entries()) {
+        matchResult.queryParams.appendParam(key, value);
+      }
+    }
+
     matchResult.result = true;
     return matchResult;
   }
--- src/modules/router/query-params.class.router.ts
+++ src/modules/router/query-params.class.router.ts
@@ -1,1 +1,37 @@
-export class QueryParams {}
+export class QueryParams {
+  private readonly params: Map<string, string | string[]>;
+
+  constructor() {
+    this.params = new Map();
+  }
+
+  setParam(key: string, value: string | string[]) {
+    this.params.set(key, value);
+  }
+
+  appendParam(key: string, value: string) {
+    const existing = this.params.get(key);
+    if (!existing) {
+      this.params.set(key, value);
+      return;
+    }
+    if (Array.isArray(existing)) {
+      existing.push(value);
+      this.params.set(key, existing);
+      return;
+    }
+    this.params.set(key, [existing, value]);
+  }
+
+  getParam(key: string): string | string[] | undefined {
+    return this.params.get(key);
+  }
+
+  getAllParams(): Record<string, string | string[]> {
+    const paramsObj: Record<string, string | string[]> = {};
+    for (const [key, value] of this.params.entries()) {
+      paramsObj[key] = value;
+    }
+    return paramsObj;
+  }
+}
